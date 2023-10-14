8d269c8c7677fba13801d470f4a23ffec9f20e56
Thanapat Jonglertthiti <thanajong@outlook.com>
2023-10-14 15:26:27 +0700

    เคสสระเสียงยาว

--- all-cases/isCase1.py
+++ all-cases/isCase1.py
@@ -1,4 +1,6 @@
 # เคส สระเสียงยาว
+# 14/10/23 ดักเคสสระเสียงสั้นไม่ออกลูป   *เพิ่มสระ-ู*   *แโ ไม่มีตัวสะกด*
+
 def isCase1(a):
     i=0
     qf = False
@@ -6,16 +8,25 @@ def isCase1(a):
         i+=1
         if(a[i] in '-่-้-๊-๋'):
             i+=1
-            if(a[i] in 'ั'):
+            if(a[i] in 'ู'):
+                qf=True
+                i+=1
+            elif(a[i] in 'ั'):
                 i+=1
                 if(a[i] in '-่-้-๊-๋'):
                     i+=1
                     if(a[i] in 'ว'):
                         qf = True
                         i+=1
+                        if((i<=len(a)-1) and a[i] in 'ะ'):
+                            qf = False
+                            i+=1
                 elif(a[i] in 'ว'):
                     qf = True
                     i+=1
+                    if((i<=len(a)-1) and a[i] in 'ะ'):
+                            qf = False
+                            i+=1
             elif(a[i] in 'วอา-ี'):
                 qf = True
                 i+=1
@@ -34,6 +45,10 @@ def isCase1(a):
                 elif(a[i] in 'กดบนงมอ'):
                     qf=True
                     i+=1
+            else: i+=1
+        elif(a[i] in 'ู'):
+                qf=True
+                i+=1
         elif(a[i] in 'ั'):
             i+=1
             if(a[i] in '-่-้-๊-๋'):
@@ -41,9 +56,16 @@ def isCase1(a):
                 if(a[i] in 'ว'):
                     qf = True
                     i+=1
+                    if((i<=len(a)-1) and a[i] in 'ะ'):
+                        qf = False
+                        i+=1
+                else: i+=1
             elif(a[i] in 'ว'):
                 qf = True
                 i+=1
+                if((i<=len(a)-1) and a[i] in 'ะ'):
+                        qf = False
+                        i+=1
         elif(a[i] in 'วอา-ี'):
             qf = True
             i+=1
@@ -62,23 +84,71 @@ def isCase1(a):
             elif(a[i] in 'กดบนงมอ'):
                 qf=True
                 i+=1
+        else: 
+            for i in range(len(a)):
+                i+=1
     elif(a[i] in 'เ'):
         i+=1
         if(a[i] in 'กขคงจฉชซดตถทนบปผฝพฟมยรลวสหอฮ'):
             qf = True
             i+=1
+            if((i<=len(a)-1) and a[i] in '่้๊๋'):
+                i+=1
+                if((i<=len(a)-1) and a[i] in 'ะ'):
+                    qf = False
+                    i+=1
+                elif((i<=len(a)-1) and a[i] in 'อ'):
+                    i+=1
+                    if((i<=len(a)-1) and a[i] in 'ะ'):
+                        qf = False
+                        i+=1
+                elif((i<=len(a)-1) and a[i] in 'า'):
+                    qf = False
+                    i+=1
+                    if((i<=len(a)-1) and a[i] in 'ะ'):
+                        qf = False
+                        i+=1
+            if((i<=len(a)-1) and a[i] in 'ะ'):
+                qf = False
+                i+=1
+            elif((i<=len(a)-1) and a[i] in 'อ'):
+                i+=1
+                if((i<=len(a)-1) and a[i] in 'ะ'):
+                    qf = False
+                    i+=1
+            elif((i<=len(a)-1) and a[i] in 'า'):
+                qf = False
+                i+=1
+                if((i<=len(a)-1) and a[i] in 'ะ'):
+                    qf = False
+                    i+=1
     elif(a[i] in 'แโ'):
         i+=1
-        if(a[i] in 'กขคงจฉชซดตถทนบปผฝพฟมยรลวสหอฮ'):
+        # if(a[i] in 'กขคงจฉชซดตถทนบปผฝพฟมยรลวสหอฮ' and (i!=len(a)-1)):
+        if(a[i] in 'กขคงจฉชซดตถทนบปผฝพฟมยรลวสหอฮ'):         
+            qf=True
             i+=1
-            if(a[i] in '-่-้-๊-๋'):
+            if((i<=len(a)-1) and a[i] in '่้๊๋'):
                 i+=1
-                if(a[i] in 'กดบนงมยว'):
+                if((i<=len(a)-1) and a[i] in 'กดบนงมยว'):
                     qf = True
                     i+=1 
-            elif(a[i] in 'กดบนงมยว'):
+                elif((i<=len(a)-1) and a[i] in 'ะ'):
+                    qf = False
+                    i+=1
+            elif((i<=len(a)-1) and a[i] in 'กดบนงมยว'):
                 qf = True
-                i+=1 
+                i+=1
+            elif((i<=len(a)-1) and a[i] in 'ะ'):
+                qf = False
+                i+=1
+            else: i+=1
+        elif( (a[i] in 'กขคงจฉชซดตถทนบปผฝพฟมยรลวสหอฮ') and (i==len(a)-1) ):
+            qf=True
+            i+=1 
+    else:
+        for i in range(len(a)):
+            i+=1
     while(i<=(len(a)-1)):
         if(qf==True):
             if(a[i] in '-่-้-๊-๋กดบนงมยว'):
@@ -97,17 +167,29 @@ def isCase1(a):
                     i+=1
                     if(a[i] in 'ย'):
                         i+=1
+                        if((i<=len(a)-1) and a[i] in 'ะ'):
+                            qf = False
+                            i+=1
                 elif(a[i] in 'ย'):
                     i+=1
+                    if((i<=len(a)-1) and a[i] in 'ะ'):
+                        qf = False
+                        i+=1
             elif(a[i] in '-ื'):
                 i+=1
                 if(a[i] in '-่-้-๊-๋'):
                     i+=1
                     if(a[i] in 'อ'):
                         i+=1
+                        if((i<=len(a)-1) and a[i] in 'ะ'):
+                            qf = False
+                            i+=1
                 elif(a[i] in 'อ'):
                         i+=1
+                        if((i<=len(a)-1) and a[i] in 'ะ'):
+                            qf = False
+                            i+=1
     return qf   
 
-word = 'เมื่อ'
+word = 'โก'
 print(isCase1(word))
